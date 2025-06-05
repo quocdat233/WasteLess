2a7f968512a5c1c6329e3ee50c54a3582fb48c14
quocdat233 <datv89242@gmail.com>
2025-06-05 10:23:34 +0700

    n

--- layout/header.js
+++ layout/header.js
@@ -21,7 +21,9 @@ function getActiveClass(linkMatchPath) {
 let menuHTML = navLinks.map(link => `
   <li><a href="${link.href}" class="${getActiveClass(link.match)}">${link.label}</a></li>
 `).join("");
-
+let mobileMenuHTML = navLinks.map(link => `
+  <li><a href="${link.href}" class="${getActiveClass(link.match)}">${link.label}</a></li>
+`).join("");
 header.innerHTML = `
 <div class="header__topbar"  data-aos="fade-down" data-aos-duration="11500">
    <div class="header__topbar container-fluid px-5 ">
@@ -125,14 +127,10 @@ header.innerHTML = `
 
 <div class="header__menu">
 <div class="header__menu__overlay"></div>
-<div class="header__menu__body--content">
-  <ul>
-    <li><a href="home.html">Home</a></li>
-    <li><a href="about.html">About</a></li>
-    <li><a href="offers.html">Offers</a></li>
-    <li><a href="news.html">News</a></li>
-    <li><a href="contact.html">Contact</a></li>
-  </ul>
+ <div class="header__menu__body--content">
+      <ul>
+        ${mobileMenuHTML}
+      </ul>
   <i class="fas fa-times" id="menu_close"></i>
 </div>
 </div>
